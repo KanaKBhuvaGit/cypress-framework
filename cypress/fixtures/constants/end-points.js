aa95203a7878cf2f5758abac75d8cf2f3a5cd20f
Kanak Bhuva <kanakbhuvagit@gmail.com>
2023-03-17 15:24:05 +0530

    Login test cases using page object model

--- cypress/fixtures/constants/end-points.js
+++
@@ -1,9 +0,0 @@
-const PATH_PREFIX = "/api"
-
-const ENDPOINTS = {
-    users: PATH_PREFIX + '/users',
-    update_user: PATH_PREFIX + '/users/2',
-    delete_user: PATH_PREFIX + '/users/2',
-};
-
-export default ENDPOINTS;
--- cypress/fixtures/constants/query-params.js
+++
@@ -1,5 +0,0 @@
-const QUERY_PARAMS = {
-    QP_PAGE: { page: 2 }
-}
-
-export default QUERY_PARAMS;
