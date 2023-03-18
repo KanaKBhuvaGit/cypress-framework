0ee767a7e932b4a4bb51980d69829f3eddf78a17
Kanak Bhuva <kanakbhuvagit@gmail.com>
2023-03-18 15:11:56 +0530

    add test cases video

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    "video": true,
     watchForFileChanges: false,
     defaultCommandTimeout: 10000,
     baseUrl: "https://reqres.in",
