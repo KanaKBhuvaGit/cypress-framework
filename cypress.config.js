00fd0f69c9a1750b17f85ef6ad5f19278e32d44f
Kanak Bhuva <kanakbhuvagit@gmail.com>
2023-12-24 15:38:17 +0530

    Updated a spec pattern and added npm command to run cypress tests

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     watchForFileChanges: false,
-    "video": false,
+    "video": true,
+    "specPattern": "cypress/e2e/1-getting-started/*.js",
     // defaultCommandTimeout: 30000,
     // screenshotOnRunFailure: false,
     setupNodeEvents(on, config) { }
